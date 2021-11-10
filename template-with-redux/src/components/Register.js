456057e620836dd8a3f0c9d48cd26b7cf15800cd
Katiuska Alicea <katiuskaalicea@gmail.com>
2021-11-09 17:33:15 -0800

    installed antd, commented out the css that comes with cra,gutted the code in App.js, created Register.js, import antd to it with rows and columsn for the layout and imported it to app.js

---
+++ template-with-redux/src/components/Register.js
@@ -0,0 +1,16 @@
+import { Row, Col } from "antd";
+
+const Register = () => {
+  return (
+    <div>
+      <Row>
+        <Col style={{ backgroundColor: "lightblue" }} flex={3}>
+          Image will go here
+        </Col>
+        <Col flex={2}>form will go here</Col>
+      </Row>
+    </div>
+  );
+};
+
+export default Register;
